75f1795f43e60e818d841de1d97f5c5ff1267aae
Boris Belmar <borisbelmarm@gmail.com>
2019-07-09 22:48:07 -0400

    Avanzando con v-bind

---
+++ 02/02.js
@@ -0,0 +1,12 @@
+const app = new Vue ({
+    el: '#app',
+    data: {
+        color: 'has-text-white',
+        primary: 'has-background-primary',
+        error: 'has-background-danger',
+        hasError: true
+    },
+    methods: {
+
+    }
+});
